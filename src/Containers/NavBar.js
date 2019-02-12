9e94868df96ad4915a755f2d2d658f8511765950
github username <github email address>
2019-02-12 09:54:01 -0500

    css and algorithm work

--- src/Containers/NavBar.js
+++ src/Containers/NavBar.js
@@ -12,12 +12,12 @@ class NavBar extends Component {
     return (
       <div className="nav">
         <Navbar fixed="top" bg="secondary" variant="dark">
-          <Navbar.Brand href="">Welcome Spy</Navbar.Brand>
-          <Button id="missionButton" fixed="right">Your Missions</Button>
+          <Navbar.Brand href="" className="navbar-brand-welcome">Welcome Spy</Navbar.Brand>
           {this.props.spy.length > 0 ?
           <Spy
             spy={this.props.spy}
             />: null}
+            <Button id="missionsButton" fixed="right">Your Missions</Button>
             <Button
               onClick={this.props.instructions}
               id="instructionsButton" bsstyle='success'>How to Play</Button>
